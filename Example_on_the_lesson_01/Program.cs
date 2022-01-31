970418deb12633f2cb3262ec442338b5f18d46f4
vitaly rotinberg <rotinberg.v@gmail.com>
2022-01-31 23:24:36 +0500

    Решили задачу по возведению числа в квадрат

---
+++ Example_on_the_lesson_01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число : ");
+
+string numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr); // функция для преобразования строки в число
+
+Console.WriteLine($"Квадрат числа = {number * number}");
