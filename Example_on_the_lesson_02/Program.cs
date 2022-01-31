8790b737d8b2a8bbbbb309056d6e97b2e4ed9b4f
vitaly rotinberg <rotinberg.v@gmail.com>
2022-01-31 23:42:43 +0500

    Решили задачу по проверке числа на квадратный корень

---
+++ Example_on_the_lesson_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число 1 : ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr); // функция для преобразования строки
+
+Console.Write("Введите число 2 : ");
+string numberStr2 = Console.ReadLine();
+int number2 = int.Parse(numberStr2);
+
+int number3 = number2 * number2;
+
+if (number3 == number)
+    Console.WriteLine ("Первое число является квадратом второго");
+    else
+    Console.WriteLine ("Первое число не является квадратом второго");
