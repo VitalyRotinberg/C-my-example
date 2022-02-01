60416a16b47b9c5cc2dba821aa4f0c2a933986cb
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-01 19:30:59 +0500

    add random number in Exmaple003_sum

--- Exmaple003_sum/Program.cs
+++ Exmaple003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 10;
-int numberB = 6;
+﻿int numberA = new Random().Next(1,10); // даст число от 1 до 9.
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
