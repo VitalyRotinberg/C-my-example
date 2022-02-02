7ce1043ca2c33a9e5d773cc5f74ba8021cb04c3b
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-02 19:24:17 +0500

    add program find to index same number

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {61,272,23,74,15,56,27,15,9};
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
