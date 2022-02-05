d79911cef0c8889b2d52d47545da21f0684a4ec7
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-05 18:39:10 +0500

    add task 4 to homework

---
+++ Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Найти максимальное из трех чисел
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(1, 100);
+    return randomNumber;
+}
+
+int MaxDigit(int firstDigit, int secondDigit)
+{
+    int max = 0;
+    if (firstDigit > secondDigit)
+        max = firstDigit;
+    else
+        max = secondDigit;
+    return max;
+}
+
+int numberOne = GetRandomNumber(), numberTwo = GetRandomNumber(), numberThree = GetRandomNumber();
+Console.WriteLine($"{numberOne}, {numberTwo}, {numberThree}");
+
+int temporary = MaxDigit(numberTwo, numberThree);
+int maxNumber = MaxDigit(numberOne, temporary);
+
+Console.WriteLine($"Максимальное из трех чисел {maxNumber}");
