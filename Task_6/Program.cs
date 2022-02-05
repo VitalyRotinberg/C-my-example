65e1ed8615f4595fc3fded619df1d3f32799397d
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-05 18:50:19 +0500

    add task 6 to homework

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Выяснить является ли число чётным
+Console.WriteLine($"Введите любое число");
+string numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+int result = number % 2;
+
+if (result == 0)
+    Console.WriteLine($"Число является четным");
+else
+    Console.WriteLine($"Число не является четным");
