f0e0860396e4d25637cd7083afe3b101d268f418
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-05 18:16:12 +0500

    add task 2 to homework

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Даны два числа. Показать большее и меньшее число
+
+Random randomaizer = new Random();
+
+int numberOne = randomaizer.Next(1, 10);
+Console.WriteLine(numberOne);
+
+int numberTwo = randomaizer.Next(1, 10);
+Console.WriteLine(numberTwo);
+
+int max = 0, min = 0;
+
+if (numberOne > numberTwo)
+{
+    max = numberOne;
+    min = numberTwo;
+    Console.WriteLine($"Большее число {max}, меньшее {min}");
+}
+if (numberOne < numberTwo)
+{
+    max = numberTwo;
+    min = numberOne;
+    Console.WriteLine($"Большее число {max}, меньшее {min}");
+}
+
+if (numberOne == numberTwo)
+    Console.WriteLine($"Оба числа равны");
