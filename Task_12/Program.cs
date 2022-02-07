6e79c35ad36181978ff514d54861a27c54ac5339
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-07 19:27:34 +0500

    added task 12 for my homework

---
+++ Task_12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Удалить вторую цифру трёхзначного числа
+int GetRandomNumber()//получаем любое трехзначное число
+{
+    int randomNumber = new Random().Next(100, 1000);
+    return randomNumber;
+}
+int Exception(int digit)//исключаем вторую цифра из трехзначного числа
+{
+    int temp = digit % 10;
+    int result = (digit / 100) * 10 + temp;
+    return result;
+}
+
+int number = GetRandomNumber();
+Console.WriteLine($"Сгенерированное число - {number}");
+
+Console.WriteLine($"Число после исключения второй цифры {Exception(number)}");
+
