82916ddbe6407fb94534167bed6969562452e88c
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-07 19:10:04 +0500

    added task 10 for my homework

---
+++ Task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать вторую цифру трёхзначного числа.
+int GetRandomNumber()//получаем любое трехзначное число
+{
+    int randomNumber = new Random().Next(100, 1000);
+    return randomNumber;
+}
+
+int number = GetRandomNumber();
+Console.WriteLine($"Сгенерированное число - {number}");
+
+int result = (number/10)%10;
+Console.WriteLine($"Вторая цифра числа {number} - {result}");
