2b34017518a1f13dbb1c2933a0a60c3ff4e225d4
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-08 17:58:05 +0500

    added task 14 for my homework

---
+++ Task_14/Program.cs
@@ -0,0 +1,44 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+int GetNumber()//получаем любое число
+{
+    Console.WriteLine($"Введите любое число");
+    string numberStr = Console.ReadLine();
+    int numberInt = int.Parse(numberStr);
+    return numberInt;
+}
+int NumberCounter(int digit)//считаем количество цифр в числе
+
+{
+    int result = digit;
+    int i = 0;
+    while (result != 0)
+    {
+        result = result / 10;
+        i++;
+    }
+    return i;
+}
+
+int number = GetNumber();
+int amountOfNumber = NumberCounter(number);
+int discharge = 10;
+int thirdNumber = 0;
+Console.WriteLine($"количество цифр {amountOfNumber}");
+
+if (amountOfNumber < 3)
+    Console.WriteLine($"Третьей цифры не обнаружено");
+
+if (amountOfNumber == 3)
+    Console.WriteLine($"Третья цифра {number % discharge}");
+
+if (amountOfNumber > 3)
+{
+    int result = amountOfNumber - 3;
+    while (result != 0)
+    {
+        discharge = discharge * 10;
+        result--;
+    }
+    thirdNumber = (number % discharge) / (discharge / 10);
+    Console.WriteLine($"Третья цифра {thirdNumber}");
+}
