77866726f4362051f2b2aaac70c79d5aa4a86cbc
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-10 22:46:11 +0500

    added task 27 on the lesson

---
+++ Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿//Определить количество цифр в числе
+//123/10 = 12/10 = 1/10 = 0
+//   0      1      2      3
+int GetNumber()//получаем число
+{
+    int randomNumber = new Random().Next(100, 10000);
+    return randomNumber;
+}
+
+int number = GetNumber();
+Console.WriteLine(number);
+
+int countOfDigit = 0;
+while (number != 0)
+{
+    number = number / 10;
+    countOfDigit++;
+}
+    
+Console.WriteLine(countOfDigit);
