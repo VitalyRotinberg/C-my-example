59cb1766f8a3e21258c9527cc174f4c82f16fb69
Виталий Ротинберг <vitalyrotinberg@MacBook-Pro-Vitalij.local>
2022-02-12 03:37:05 +0300

    added task 18 for my homework

---
+++ Task_18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+// ¬ Логическое отрицание. Если Значение было false, то после логического отрицания оно становится true, и наоборот. Условный оператор выглядит так: (!*переменна*)
+
+// Для начала напишем метод, который рандомно будет инициализировать в переменные true или false.
+bool GetRandomBool()
+{
+    int randomNumber = new Random().Next(0, 2);
+    if (randomNumber == 1)
+        return true;
+    else
+        return false;
+}
+
+// Создаем две переменные Х и Y, они будут равны результату выполненной функции.
+
+bool x = GetRandomBool();
+Console.WriteLine($"X имеет значение {x}");
+
+bool y = GetRandomBool();
+Console.WriteLine($"Y имеет значение {y}");
+
+// Проверяем истинность утверждения.
+
+bool result = !(x || y) == ((!x) && (!y));
+if (result)
+    Console.WriteLine("Утверждение истинно");
+else
+    Console.WriteLine("Утверждение ложное");
