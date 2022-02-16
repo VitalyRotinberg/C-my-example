3c15e91288a36b9ad02ceffd161a9430c76034fc
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-17 00:52:17 +0500

    added task 32 for my homework

---
+++ Task_32/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран.
+
+// Первым делом напишу метод, который будет генерить либо 0, либо 1
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(0, 2);
+    return randomNumber;
+}
+
+// Далее пишем метод, который будет заполнять массив.
+void FillArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = GetRandomNumber();
+    }
+}
+
+// Последним этапом выводим на экран заполненный массив.
+void PrintArray (int [] array)
+{
+     for (int i = 0; i < array.Length; i++)
+     {
+        Console.Write($"{array[i]} ");
+     }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
