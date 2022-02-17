c22ca244ff8dd58223a27fbceaddcf9a18575316
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-17 22:32:36 +0500

    add task 41 for the lesson

---
+++ Task_41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+// Получаем три числа
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(1, 10);
+    return randomNumber;
+}
+
+// сравниваем число с суммой остальных
+
+bool SumTwoNumber(int a, int b, int c)
+{
+    return a < (b + c);
+}
+
+int numberOne = GetRandomNumber();
+int numberTwo = GetRandomNumber();
+int numberThree = GetRandomNumber();
+Console.WriteLine($"Первое число - {numberOne}, Второе число - {numberTwo}, Третье число - {numberThree}");
+
+if (SumTwoNumber(numberOne,numberTwo,numberThree) 
+&& SumTwoNumber(numberTwo, numberOne, numberThree)
+&& SumTwoNumber(numberThree, numberOne, numberTwo))
+    Console.WriteLine("Числа являются сторонами треугольника");
+else
+    Console.WriteLine("Числа не являются сторонами треугольника");
+
