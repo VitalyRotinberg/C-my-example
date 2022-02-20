a6254f9e49b6abb7808f35d65e13fa04e464b6cf
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-20 16:26:14 +0500

    add task 36 for my homework

---
+++ Task_36/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] array = new int[10];
+
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(100, 1000);
+    return randomNumber;
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = GetRandomNumber();
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void EvenOddNumberCounter(int[] array)
+{
+    int even = 0;
+    int odd = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+            even++;
+        else
+            odd++;
+    }
+    Console.WriteLine($"Количество четных чисел в массиве {even}");
+    Console.WriteLine($"Количество нечетных чисел в массиве {odd}");
+}
+
+FillArray(array);
+PrintArray(array);
+EvenOddNumberCounter(array);
