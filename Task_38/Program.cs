fa988aa25728b117ac3b541393d84a7074eaef61
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-20 16:37:24 +0500

    add task 38 for my homework

---
+++ Task_38/Program.cs
@@ -0,0 +1,34 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] arr = new int[20];
+
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(1, 1000);
+    return randomNumber;
+}
+
+// Надоело два метода вызывать, все в одном пусть будет)
+void FillAndPrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = GetRandomNumber();
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SumOddPositionArray(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+            result = result + arr[i];
+    }
+    Console.WriteLine($"Сумма элементов массива на нечетной позиции = {result}");
+}
+
+FillAndPrintArray(arr);
+SumOddPositionArray(arr);
