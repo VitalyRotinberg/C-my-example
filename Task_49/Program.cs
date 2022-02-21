75cdcaaa9f2475908eb63a783a3b8ba6ced707d2
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-21 22:52:50 +0500

    add task 49 for my homework

---
+++ Task_49/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+double [,] arr = new double[6,5];
+
+Random randomDouble = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = Math.Round(randomDouble.NextDouble() * 10, 3);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray();
