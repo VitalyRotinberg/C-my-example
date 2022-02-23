875ad3f308b3757150536e152a453edd9f6820e2
vitaly rotinberg <rotinberg.v@gmail.com>
2022-02-23 20:17:28 +0500

    add task 46 for the homework

---
+++ Task_46/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу масштабирования фигуры
+string strValue = "(0,0) (2,0) (2,2) (0,2)";
+Console.WriteLine("Введите множитель: ");
+int k = int.Parse(Console.ReadLine());
+
+//Удаляем "(" и "), и ","
+strValue = strValue.Replace("(", "").Replace(")", "").Replace(",", " ");
+
+//Разбиваем строку на массив, разделить " " - пробел
+string[] strArray = strValue.Split(" ");
+
+string result = string.Empty;
+
+foreach (var item in strArray)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < strArray.Length - 1; i = i + 2)
+{
+    int x = k * int.Parse(strArray[i]);
+    int y = k * int.Parse(strArray[i + 1]);
+
+    result = result + $"({x}, {y})";
+}
+
+Console.WriteLine(result);
