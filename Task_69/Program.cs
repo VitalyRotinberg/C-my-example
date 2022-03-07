33eb170d54a3d1e33d43f0bb7cb5422876337526
vitaly rotinberg <rotinberg.v@gmail.com>
2022-03-07 14:54:44 +0500

    add task 69 for the homework

---
+++ Task_69/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int result = 0;
+
+for (int i = m; i <= n; i++)
+{
+    result = result + i;
+}
+Console.WriteLine(result);
