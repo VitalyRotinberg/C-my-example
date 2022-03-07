cbc847d11031374bd75c9b413511292a4126b47c
vitaly rotinberg <rotinberg.v@gmail.com>
2022-03-07 14:51:17 +0500

    add task 67 for the homework

---
+++ Task_67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int result = m + 1;
+
+Console.Write($"{m} ");
+
+for (int i = m; i < n; i++)
+{
+    Console.Write($"{result} ");
+    result++;
+}
